e88bc3ec486a9dd1d398829e6f7bdd68831a3526
xuannn0915 <xuannn0915@gmail.com>
2024-01-18 11:56:50 +0800

    新增產品modal
    建立新增產品modal左側樣式與畫面

--- Week3/js/backstage.js
+++ Week3/js/backstage.js
@@ -2,32 +2,11 @@
 const url = 'https://ec-course-api.hexschool.io/v2';
 const api_path='xuan02';
 
-const productList =[
-  {
-      "title": "柳橙汁",
-      "category": "飲品",
-      "origin_price": 30,
-      "price": 20,
-      "unit": "瓶",
-      "description": "柳橙汁好喝",
-      "content": "這是一瓶很好喝的柳橙汁",
-      "is_enabled": 20,
-      "imageUrl": "https://images.unsplash.com/photo-1600271886742-f049cd451bba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      "imagesUrl": [
-        "https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1621506289894-c3a62d6be8f3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      ]
-    }
-]
-
 const app = {
   data() {
     return {
       products: [],
-      detail:{},
       isChosenHint:true,
-      username:'',
-      password:'',
     };
   },
   methods: {
@@ -35,21 +14,16 @@ const app = {
       axios.get(`${url}/api/${api_path}/admin/products`)
       .then((res)=>{
         this.products = res.data.products;
-        console.log(this.products);
       })
       .catch((err)=>{
         console.log(err.response);
       })
     },
-    getProduct(id){
-      this.products.forEach((item)=>{
-        if(id===item.id){
-          this.detail={...item};
-        }
-      });
-      this.isChosenHint=false;
-    },
-      
+    addProductModal(){
+      const addProduct = document.querySelector('#addProduct');
+      const addProductModal = new bootstrap.Modal(addProduct);
+      addProductModal.show();
+    }      
   },
   mounted() {
     const token = document.cookie.replace(
@@ -60,7 +34,8 @@ const app = {
 
     axios.post(`${url}/api/user/check`)
     .then((res)=>{
-      this.renderProducts()
+      this.renderProducts();
+      this.addProductModal()
     })
     .catch((err)=>{
       alert(err.response.data.message);
