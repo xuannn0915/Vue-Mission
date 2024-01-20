648e0a4c1dfd66d2f25b2d55699ae163539d850e
xuan <xuannn0915@gmail.com>
2024-01-20 15:46:09 +0800

    modal finish
    完成modal新增編輯及刪除，借下來進行modal中新增圖片的邏輯

--- Week3/js/backstage.js
+++ Week3/js/backstage.js
@@ -8,7 +8,7 @@ const app = {
   data() {
     return {
       products: [],
-      isNew:false,
+      isNew: false,
       tempProduct: {
         imagesUrl: [],
       },
@@ -43,30 +43,27 @@ const app = {
     },
 
     // 開啟modal
-    openModal(status, item){
+    openModal(status, item) {
       // 先判斷狀態是「新增／編輯」，在帶入選擇的物件
-      if(status=='new'){
-        this.tempProduct={
+      if (status == "new") {
+        this.tempProduct = {
           imagesUrl: [],
         };
-        this.isNew=true;
+        this.isNew = true;
         productModal.show();
-      }
-      else if(status=='edit'){
-        this.tempProduct={...item};
-        this.isNew=false;
+      } else if (status == "edit") {
+        this.tempProduct = { ...item };
+        this.isNew = false;
         productModal.show();
-      }
-      else if(status == 'delete'){
-        this.tempProduct = {...item};
-        this.isNew =false;
-        deleteModal.show()
+      } else if (status == "delete") {
+        this.tempProduct = { ...item };
+        deleteModal.show();
       }
     },
 
     // 確認後執行的動作(儲存／修改)
-    saveProduct() {
-      if(this.isNew == true){
+    saveProduct(id) {
+      if (this.isNew == true) {
         axios
           .post(`${url}/api/${api_path}/admin/product`, {
             data: this.tempProduct,
@@ -79,8 +76,35 @@ const app = {
           .catch((err) => {
             alert(err.response.data.message);
           });
+      } else if (this.isNew == false) {
+        axios
+          .put(`${url}/api/${api_path}/admin/product/${id}`, {
+            data: this.tempProduct,
+          })
+          .then((res) => {
+            alert(res.data.message);
+            productModal.hide();
+            this.renderProducts();
+          })
+          .catch((err) => {
+            console.log(err.response);
+          });
       }
     },
+
+    // 刪除
+    delProduct(id){
+        axios
+          .delete(`${url}/api/${api_path}/admin/product/${id}`)
+          .then((res) => {
+            alert(res.data.message);
+            deleteModal.hide();
+            this.renderProducts();
+          })
+          .catch((err) => {
+            console.log(err.response);
+          });
+      }
   },
 
   // 初始化
@@ -95,11 +119,10 @@ const app = {
     // modal初始化才能抓到dom元素
     productModal = new bootstrap.Modal(document.getElementById("addModal"));
     deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-    
-    // 確認身份
-    this.checkUser()
-  },
 
+    // 確認身份
+    this.checkUser();
+  },
 };
 
 Vue.createApp(app).mount("#app");
