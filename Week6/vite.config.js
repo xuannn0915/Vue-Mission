13a5500e45c446cd72be0832cd53438f17dd5713
xuan <xuannn0915@gmail.com>
2024-02-21 00:30:08 +0800

    base test

--- Week6/vite.config.js
+++ Week6/vite.config.js
@@ -9,7 +9,8 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  // base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  base: '/Vue-Mission/Week6/',
   plugins: [
     vue(),
     eslintPlugin({
